380d8f8ee8458f5c839b183588312f3d6a497e8f
Jonathan Vega <31710893+JonVega@users.noreply.github.com>
2022-10-31 08:34:17 +0900

    player can now look
    added looking in the Update() instead of FixedUpdate() using the NewInputSystem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,13 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerInputActions playerInputActions;
     private InputAction movement;
+    private InputAction jump;
+    private InputAction looking;
 
+    public Transform playerBody;
+    public Camera cam;
+    public float mouseSensitivity = 80f;
+    private float xRotation = 0f;
 
     private void Awake() {
         //note: inputAction asset is not static or global
@@ -14,23 +20,50 @@ public class PlayerController : MonoBehaviour
 
     private void OnEnable() {
         movement = playerInputActions.Player.Movement;
-        movement.Enable(); //must call this function or input action won't work
+        movement.Enable(); //must call Enable() function or else input action won't work
 
-        playerInputActions.Player.Jump.performed += DoJump;
-        playerInputActions.Player.Jump.Enable();
+        jump = playerInputActions.Player.Jump;
+        jump.Enable();
+
+        looking = playerInputActions.Player.MouseDelta;
+        looking.Enable();
     }
 
     private void OnDisable() {
         movement.Disable();
-        playerInputActions.Player.Jump.Disable();
+        jump.Disable();
+        looking.Disable();
+    }
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked; //hides mouse cursor in game
+        jump.performed += DoJump; //subscribe to event
+    }
+
+    private void FixedUpdate() //use physics engine to move and control player object
+    {
+
+    }
+
+    private void Update() {
+        Move();
+        Look();
     }
 
     private void DoJump(InputAction.CallbackContext obj) {
         Debug.Log("Jump!");
     }
 
-    private void FixedUpdate() //use physics engine to move and control player object
-    {
+    private void Look() {
+        float mouseX = looking.ReadValue<Vector2>().x * mouseSensitivity * Time.deltaTime;
+        float mouseY = looking.ReadValue<Vector2>().y * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * looking.ReadValue<Vector2>().x);
+    }
+
+    private void Move() {
         Debug.Log("Movement Values: " + movement.ReadValue<Vector2>());
     }
 }
